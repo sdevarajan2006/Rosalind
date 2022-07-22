623b6ad147d68a0188189523b4d062efaacb9898
Sanjana Devarajan <sdevarajan2006@gmail.com>
2022-07-22 12:11:28 -0500

    new module

---
+++ test.py
@@ -0,0 +1,29 @@
+import profile_and_consensus as pc
+
+sample = """>Rosalind_1
+  ATCCAGCT
+  >Rosalind_2
+  GGGCAACT
+  >Rosalind_3
+  ATGGATCT
+  >Rosalind_4
+  AAGCAACC
+  >Rosalind_5
+  TTGGAACT
+  >Rosalind_6
+  ATGCCATT
+  >Rosalind_7
+  ATGGCACT"""
+
+pc.profile_and_consensus(sample)
+
+
+s = [2,3,4,4,2,4,4,5]
+print(len(s))
+
+print(s[0])
+
+sanjana  = 'sanjana'
+print(sanjana[0:2])
+
+print('sanjana'[:-1])
